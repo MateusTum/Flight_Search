3bb6550acaf14835bb9051512d99ce3d1e8ebdc4
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-03-03 12:30:59 -0300

    Add urls to main urls.py

--- flight_monitor/urls.py
+++ flight_monitor/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+        # Include the app's URLs
+    path('monitor/', include('monitor.urls')),
+    # You can include URLs from other apps similarly
+    path('flights/', include('flights.urls')),
 ]
